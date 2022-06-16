cede3abf45a8500221b5a33f84b649aa96c460cf
Zabalitee <101581357+Zabalitee@users.noreply.github.com>
2022-06-16 15:48:50 +0200

    Time: 3 ms (53.22%), Space: 42.3 MB (45.53%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -2,8 +2,7 @@ import java.util.Arrays;
 
 class Solution {
  public char findTheDifference(String s, String t) {
-        char[] one = s.toCharArray();
-        char[] two = t.toCharArray();
+     
         int sum=0;
         for(int i=0;i<s.length();i++){
             sum += s.codePointAt(i);
@@ -14,19 +13,7 @@ class Solution {
         }
         int f = sumT-sum;
 
-        Arrays.sort(one);
-        char v = '1';
-        Arrays.sort(two);
-        for (int i = 0; i < t.length(); i++) {
-            for (int j = 0; j < s.length(); j++) {
-                if (two[i] == one[j]) {
-                    one[j] = 0;
-                    break;
-                } else {
-                    v = two[i];
-                }
-            }
-        }
+       
 
         return  Character.toString(f).toCharArray()[0] ;
 
