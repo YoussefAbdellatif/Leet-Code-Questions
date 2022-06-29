01edb3683ea0ff783647123173597361e16d6717
Zabalitee <101581357+Zabalitee@users.noreply.github.com>
2022-06-29 17:47:25 +0200

    Time: 3 ms (6.56%), Space: 41.7 MB (67.57%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,17 @@
+class Solution {
+        public int strStr(String haystack, String needle) {
+        if(needle.length() ==0){
+            return 0;
+        }
+        for (int i = 0; i < haystack.length(); i++) {
+            char[] f= Arrays.copyOfRange(haystack.toCharArray(),i,i+needle.length());
+            char[] b=needle.toCharArray();
+            if(Arrays.equals(f,b)){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+}
